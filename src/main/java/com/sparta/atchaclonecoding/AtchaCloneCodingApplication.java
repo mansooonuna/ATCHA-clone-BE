730e4bd47943bef7437a42d633e960cc274a7435
Dunkin00 <meister630@gmail.com>
2023-05-12 19:12:56 +0900

    initial commit

---
+++ src/main/java/com/sparta/atchaclonecoding/AtchaCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.atchaclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtchaCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtchaCloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/atchaclonecoding/AtchaCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.atchaclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtchaCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
