d59443f0649f4e58706e7a0bda098044272b6e7e
Dunkin00 <meister630@gmail.com>
2023-05-12 23:14:08 +0900

    Feat : Member 회원가입, 로그인 기능 추가
    Refresh, Access Token 적용
    JwtUtil, JwtAuthFilter 설정
    UserDetailsImpl 수정

---
+++ src/main/java/com/sparta/atchaclonecoding/domain/member/dto/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.atchaclonecoding.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
--- src/main/java/com/sparta/atchaclonecoding/security/userDetails/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/atchaclonecoding/security/userDetails/UserDetailsServiceImpl.java
@@ -1,4 +1,6 @@
 package com.sparta.atchaclonecoding.security.userDetails;
+import com.sparta.atchaclonecoding.domain.member.entity.Member;
+import com.sparta.atchaclonecoding.domain.member.repository.MemberRepository;
 import com.sparta.atchaclonecoding.security.userDetails.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,10 +15,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-
         return new UserDetailsImpl(member, member.getEmail());
     }
 }
