7dd46908397d833ea9f233fdc7667231fe02c793
Dunkin00 <meister630@gmail.com>
2023-05-14 00:02:33 +0900

    Feat : Cors 정책 추가

--- src/main/java/com/sparta/atchaclonecoding/security/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/atchaclonecoding/security/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sparta.atchaclonecoding.security.config;
 
 import com.sparta.atchaclonecoding.security.jwt.JwtAuthFilter;
+import com.sparta.atchaclonecoding.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -47,5 +51,26 @@ public class WebSecurityConfig {
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000");
+
+        config.addExposedHeader(JwtUtil.ACCESS_KEY);
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
