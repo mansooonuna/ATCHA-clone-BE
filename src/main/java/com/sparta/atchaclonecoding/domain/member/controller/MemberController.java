de226a9acd8761e8401ee9224c264ac6f3647d59
mansooonuna <parkdasom95@gmail.com>
2023-05-13 22:26:58 +0900

    Feat : 회원가입 유효성 검사 먼저 추가함

--- src/main/java/com/sparta/atchaclonecoding/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/atchaclonecoding/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.sparta.atchaclonecoding.domain.member.dto.SignupRequestDto;
 import com.sparta.atchaclonecoding.domain.member.service.MemberService;
 import com.sparta.atchaclonecoding.util.Message;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +22,14 @@ public class MemberController {
 
     //회원가입
     @PostMapping("/signup")
-    public ResponseEntity<Message> signup(@RequestBody SignupRequestDto requestDto){
+    public ResponseEntity<Message> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         return memberService.signup(requestDto);
     }
 
     //로그인
     @PostMapping("/login")
-    public ResponseEntity<Message> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response){
+    public ResponseEntity<Message> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
         return memberService.login(requestDto, response);
     }
-
 
 }
--- src/main/java/com/sparta/atchaclonecoding/domain/member/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/atchaclonecoding/domain/member/dto/SignupRequestDto.java
@@ -1,12 +1,20 @@
 package com.sparta.atchaclonecoding.domain.member.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class SignupRequestDto {
+
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$")
     private String email;
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d|.*[!@#$%^&*()\\-+=~])(?=.*[A-Za-z\\\\d!@#$%^&*()\\-+=~]).{10,}$", message = "영문, 숫자, 특문 중 2개 조합 10자 이상")
     private String password;
+    @Size(min = 2, message = "2글자 이상")
     private String nickname;
 }
+
+
