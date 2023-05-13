a1d42b2030a4dc26800d4e475a5e05bd18fa6605
mansooonuna <parkdasom95@gmail.com>
2023-05-14 00:01:55 +0900

    Refactor : 회원가입 시 유효성 검증 정규식 변경

--- src/main/java/com/sparta/atchaclonecoding/domain/member/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/atchaclonecoding/domain/member/dto/SignupRequestDto.java
@@ -4,12 +4,14 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class SignupRequestDto {
 
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$")
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$")
     private String email;
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d|.*[!@#$%^&*()\\-+=~])(?=.*[A-Za-z\\\\d!@#$%^&*()\\-+=~]).{10,}$", message = "영문, 숫자, 특문 중 2개 조합 10자 이상")
     private String password;
