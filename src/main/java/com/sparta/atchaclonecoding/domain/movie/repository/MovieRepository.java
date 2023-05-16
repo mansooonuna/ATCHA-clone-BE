7194ca58e872d63d42d57ac00d21cf796d3efaf5
mansooonuna <parkdasom95@gmail.com>
2023-05-16 11:43:26 +0900

    Refactor : Movie 도메인 media로 편입

--- src/main/java/com/sparta/atchaclonecoding/domain/movie/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.atchaclonecoding.domain.movie.repository;
-
-import com.sparta.atchaclonecoding.domain.movie.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
