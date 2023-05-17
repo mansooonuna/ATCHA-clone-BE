67d07a3430e1180ca6ee0e117398f58e24b5eef5
mansooonuna <parkdasom95@gmail.com>
2023-05-17 10:53:27 +0900

    Resolves : 카테고리도 같이 반환하도록 추가

--- src/main/java/com/sparta/atchaclonecoding/domain/media/dto/TvDetailResponseDto.java
+++ src/main/java/com/sparta/atchaclonecoding/domain/media/dto/TvDetailResponseDto.java
@@ -2,6 +2,7 @@ package com.sparta.atchaclonecoding.domain.media.dto;
 
 import com.sparta.atchaclonecoding.domain.casting.dto.CastingResponseDto;
 import com.sparta.atchaclonecoding.domain.media.entity.Media;
+import com.sparta.atchaclonecoding.domain.media.entity.MediaType;
 import com.sparta.atchaclonecoding.domain.review.dto.ReviewResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,26 +13,28 @@ import java.util.List;
 @NoArgsConstructor
 public class TvDetailResponseDto {
     private Long tvId;
+    private MediaType category;
     private String title;
     private double star;
     private String genre;
     private String age;
     private String image;
     private String information;
-    private List<CastingResponseDto> castingResponseDtos;
-    private List<ReviewResponseDto> reviewResponseDtos;
+    private List<CastingResponseDto> castingList;
+    private List<ReviewResponseDto> reviewList;
 
 
-    public TvDetailResponseDto(Media media, List<ReviewResponseDto> reviewResponseDtos, List<CastingResponseDto> castingList) {
+    public TvDetailResponseDto(Media media, List<ReviewResponseDto> reviewList, List<CastingResponseDto> castingList) {
         this.tvId = media.getId();
+        this.category = media.getCategory();
         this.title = media.getTitle();
         this.image = media.getImage();
         this.genre = media.getGenre();
         this.star = media.getStar();
         this.age = media.getAge();
         this.information = media.getInformation();
-        this.castingResponseDtos = castingList;
-        this.reviewResponseDtos = reviewResponseDtos;
+        this.castingList = castingList;
+        this.reviewList = reviewList;
     }
 }
 
