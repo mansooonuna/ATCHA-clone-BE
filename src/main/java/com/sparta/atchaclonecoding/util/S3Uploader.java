55e567d75896f1ffe71f81f56c1d54626749ccf6
Hohomii <glgltjsal@naver.com>
2023-05-18 15:18:13 +0900

    Refactor : 다운받은 크롤링 이미지 S3에 업로드

--- src/main/java/com/sparta/atchaclonecoding/util/S3Uploader.java
+++ src/main/java/com/sparta/atchaclonecoding/util/S3Uploader.java
@@ -1,8 +1,11 @@
 package com.sparta.atchaclonecoding.util;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +16,14 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
+@Log4j2
 public class S3Uploader {
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    // 프로필 업로드
     public String uploadFile(MultipartFile multipartFile) throws IOException {
         String fileName = UUID.randomUUID().toString() + "_" + multipartFile.getOriginalFilename();
 //        if (!fileName.toLowerCase().endsWith(".jpg")) {
@@ -31,6 +36,23 @@ public class S3Uploader {
         return amazonS3.getUrl(bucket, fileName).toString();
     }
 
+    public String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    // 삭제
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    //
     public String uploadImage(String image){
         ObjectMetadata objMeta = new ObjectMetadata();
         objMeta.setContentLength(image.length());
@@ -39,6 +61,12 @@ public class S3Uploader {
         return amazonS3.getUrl(bucket, image).toString();
     }
 
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    // 프로필 삭제
     public boolean delete(String fileUrl) {
         try {
             String[] temp = fileUrl.split("/");
